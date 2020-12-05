56231f34f807af8492a22acb5fba828c8028668c
Chris Van Law <chris@chrisvanlaw.com>
2020-12-05 12:37:00 -0700

    feat: day 2 puzzle 1 solved

---
+++ src/Entities/DayTwoSolver.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AoC2020.Entities
+{
+    public class DayTwoSolver
+    {
+        private readonly string _inputFile;
+
+        public DayTwoSolver(string puzzleInput)
+        {
+            if (!File.Exists(puzzleInput))
+            {
+                throw new ArgumentException($"Unable to read file {puzzleInput}!");
+            }
+
+            this._inputFile = puzzleInput;
+        }
+
+        public async Task<string> SolveAsync()
+        {
+            var lines = new List<DayTwoLine>();
+            var builder = new StringBuilder();
+
+            using (var reader = new StreamReader(this._inputFile))
+            {
+                string line = null;
+                while ((line = await reader.ReadLineAsync().ConfigureAwait(false)) != null)
+                {
+                    lines.Add(new DayTwoLine(line));
+                }
+            }
+
+            Console.WriteLine($"Searching {lines.Count} lines in file...");
+            var validCount = lines.Count(x => x.IsValid());
+            builder.Append($"1st Answer: {validCount}");
+
+            return builder.ToString();
+        }
+
+        private class DayTwoLine
+        {
+            public DayTwoLine(string input)
+            {
+                var match = (new Regex(@"((\d*)-(\d*))\s([a-z|A-Z]):\s(\w*)", RegexOptions.Compiled)).Match(input);
+
+                this.MinimumCount = int.Parse(match.Groups[2].ToString());
+                this.MaximumCount = int.Parse(match.Groups[3].ToString());
+                this.Character = char.Parse(match.Groups[4].ToString());
+                this.Password = match.Groups[5].ToString();
+                this.Raw = input;
+            }
+
+            public int MinimumCount { get; set; }
+
+            public int MaximumCount { get; set; }
+
+            public char Character { get; set; }
+
+            public string Password { get; set; }
+
+            public string Raw { get; set; }
+
+            public bool IsValid()
+            {
+                var matchingCharacters = this.Password.Where(c => c == this.Character).ToList();
+                return matchingCharacters.Count >= this.MinimumCount && matchingCharacters.Count <= this.MaximumCount;
+            }
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -12,8 +12,12 @@ namespace AoC2020
         static async Task Main(string[] args)
         {
             var dayOneSolver = new DayOneSolver("../puzzle_input/day_1.txt");
-            var solution = await dayOneSolver.SolveAsync();
+            var solution = await dayOneSolver.SolveAsync().ConfigureAwait(false);
             Console.WriteLine($"Day 1: {solution}.");
+
+            var dayTwoSolver = new DayTwoSolver("../puzzle_input/day_2.txt");
+            var solution2 = await dayTwoSolver.SolveAsync().ConfigureAwait(false);
+            Console.WriteLine($"Day 2: {solution2}.");
         }
     }
 }
