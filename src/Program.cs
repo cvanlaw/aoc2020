ea651b7bfdfa5553489dbda5019d0701feb40cbc
Chris Van Law <chris@chrisvanlaw.com>
2020-12-05 11:41:56 -0700

    feat: day 1 puzzle 1 complete

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AoC2020
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = new List<int>();
+            using (var reader = new StreamReader("../puzzle_input/day_1.txt"))
+            {
+                string line = null;
+                while ((line = await reader.ReadLineAsync().ConfigureAwait(false)) != null)
+                {
+                    lines.Add(int.Parse(line));
+                }
+            }
+
+            Console.WriteLine($"Searching {lines.Count} lines in file...");
+
+            for (var i = 0; i < lines.Count - 1; i++)
+            {
+                var thisLine = lines[i];
+
+                for (var j = i + 1; j < lines.Count - 1; j++)
+                {
+                    var nextLine = lines[j];
+
+                    if (thisLine + nextLine == 2020)
+                    {
+                        Console.WriteLine($"Answer: {thisLine * nextLine}");
+                        break;
+                    }
+                }
+            }
         }
     }
 }
