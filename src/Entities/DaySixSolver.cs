95e8f64ca2d04a4771909068de26a6777eb2fc3a
Chris Van Law <chris@chrisvanlaw.com>
2020-12-07 21:28:59 -0700

    feat: day 6 puzzle 1 complete

---
+++ src/Entities/DaySixSolver.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AoC2020.Entities
+{
+    public class DaySixSolver
+    {
+        private readonly string _inputFile;
+
+        public DaySixSolver(string puzzleInput)
+        {
+            if (!File.Exists(puzzleInput))
+            {
+                throw new ArgumentException($"Unable to read file {puzzleInput}!");
+            }
+
+            this._inputFile = puzzleInput;
+        }
+
+        public async Task<string> SolveAsync()
+        {
+            var lines = new List<(int, Dictionary<char, int>)>();
+            var innerDictionary = new Dictionary<char, int>();
+            var builder = new StringBuilder();
+
+            using (var reader = new StreamReader(this._inputFile))
+            {
+                string line = null;
+                var numberOfPersons = 0;
+
+                while ((line = await reader.ReadLineAsync().ConfigureAwait(false)) != null)
+                {
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        lines.Add((numberOfPersons, innerDictionary));
+                        innerDictionary = new Dictionary<char, int>();
+                        numberOfPersons = 0;
+                        continue;
+                    }
+
+                    numberOfPersons++;
+                    foreach (var character in line.ToArray())
+                    {
+                        if (!innerDictionary.ContainsKey(character))
+                        {
+                            innerDictionary.Add(character, 0);
+                        }
+
+                        innerDictionary[character]++;
+                    }
+                }
+
+                if (innerDictionary.Any())
+                {
+                    lines.Add((numberOfPersons, innerDictionary));
+                }
+            }
+
+            var totalYesAnswers = lines
+                .Sum(group => group.Item2.Count());
+            builder.Append($"\n1st Answer: {totalYesAnswers}");
+
+            return builder.ToString();
+        }
+
+
+    }
+}
